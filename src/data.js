9169c017ce7ec416021ebc3271da3fe566d4a77f
lmyEsther <esthercheremo@gmail.com>
2018-12-19 23:16:51 -0500

    primeros tests funcionan, primera historia

--- src/data.js
+++ src/data.js
@@ -1,75 +1,18 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-/*const example = () => {
-  return 'example';
+const recentYears = (arr) => {
+  return arr.filter(ele => parseInt(ele.Year) >= 2010 & parseInt(ele.Year) <= 2015);
 };
 
-window.example = example;*/
+const obtenerAñosUnicos = (arr) => {
+  return arr.reduce((acum, ele) => {
+    acum.push(parseInt(ele.Year));
+    return acum;
+  }, []);
+};
+const filtrarRango = () => {};
 
-const recentYear = INJURIES.filter(INJURIE =>
-  parseInt(INJURIE.Year) >= 2010 & parseInt(INJURIE.Year) <= 2015);
+window.injuries = {
+  recentYears,
+  obtenerAñosUnicos,
+  filtrarRango
+};
 
-window.recentYear = recentYear;
-
-/*let medioMaritimo = INJURIES.map(INJURIE =>
-      INJURIE.Total_Injured_Persons_Freight_Vessel
-      INJURIE.Total_Injured_Persons_Passenger_Vessel +
-      INJURIE.Total_Injured_Persons_Water +
-      INJURIE.Total_Injured_Persons_Water_Not_Related_To_Vessel_Casualties +
-      INJURIE.Total_Injured_Persons_Water_Vessel_Related +
-      INJURIE.Total_Injured_Persons_Recreational_Boating
-);
-
-let medioAereo = INJURIES.map(INJURIE =>
-      INJURIE.Total_Injured_Persons_Air +
-      INJURIE.Total_Injured_Persons_General_Aviation +
-      INJURIE.Total_Injured_Persons_On_Demand_Air_Taxi +
-      INJURIE.Total_Injured_Persons_US_Air_Carrier
-);
-let medioTerreste = INJURIES.map(INJURIE =>
-      INJURIE.Total_Injured_Persons_Highway +
-      INJURIE.Total_Injured_Persons_Bus_Occupants +
-      INJURIE.Total_Injured_Persons_Motorcyclists +
-      INJURIE.Total_Injured_Persons_Other_Incident_Transit +
-      INJURIE.Total_Injured_Persons_Highway_Rail_Grade_Crossing +
-      INJURIE.Total_Injured_Persons_Passenger_Car_Occupants +
-      INJURIE.Total_Injured_Persons_Passenger_Or_Occupant +
-      INJURIE.Total_Injured_Persons_Pedalcyclists +
-      INJURIE.Total_Injured_Persons_Pedestrians +
-      INJURIE.Total_Injured_Persons_Transit_Total +
-      INJURIE.Total_Injured_Persons_Trespassers +
-      INJURIE.Total_Injured_Persons_Truck_Occupants_Large +
-      INJURIE.Total_Injured_Persons_Truck_Occupants_Light +
-      INJURIE.Total_Injured_Persons_Commuter_Carrier
-);
-
-let medioFerroviario = INJURIES.map(INJURIE =>
-      INJURIE.Total_Injured_Persons_Highway_Rail_Grade_Crossing_Railroads +
-      INJURIE.Total_Injured_Persons_Other_Incidents_Railroads +
-      INJURIE.Total_Injured_Persons_Rail +
-      INJURIE.Total_Injured_Persons_Rail_Freight +
-      INJURIE.Total_Injured_Persons_Rail_Freight_Other_Incidents +
-      INJURIE.Total_Injured_Persons_Rail_Freight_Train_Accidents +
-      INJURIE.Total_Injured_Persons_Rail_Freight_Trespassers +
-      INJURIE.Total_Injured_Persons_Rail_Road_Trespassers +
-      INJURIE.Total_Injured_Persons_Railroad +
-      INJURIE.Total_Injured_Persons_Railroad_Alone +
-      INJURIE.Total_Injured_Persons_Railroad_Train_Accident +
-      INJURIE.Total_Injured_Persons_Train_Accidents_Rail_Roads +
-      INJURIE.Total_Injured_Persons_Transit_Non_Rail +
-      INJURIE.Total_Injured_Persons_Transit_Rail +
-      INJURIE.Total_Injured_Persons_Rail_Freight_Highway_Rail_Grade_Crossing +
-
-);
-
-let otrosMedios = INJURIES.map(INJURIE =>
-      INJURIE.Total_Injured_Persons +
-      INJURIE.Total_Injured_Persons_Employee_Or_Worker +
-      INJURIE.Total_Injured_Persons_Gas_Pipeline +
-      INJURIE.Total_Injured_Persons_Hazardous_Liquid_Pipeline +
-      INJURIE.Total_Injured_Persons_Industrial_Or_Other +
-      INJURIE.Total_Injured_Persons_Other_Incident +
-      INJURIE.Total_Injured_Persons_Other_Incidents +
-      INJURIE.Total_Injured_Persons_Pipeline
-); */
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,6 +1,53 @@
+/* eslint-disable camelcase */
 require('../src/data.js');
 
+const input = [{Year: '1992-01-04'}, {Year: '1993-01-04'}, {Year: '1994-01-04'}, {Year: '1995-01-04'}, {Year: '1996-01-04'}];
+const output = [1992, 1993, 1994, 1995, 1996];
 
+const input2 = [{Total_Injured_Persons: 2237367, Total_Injured_Persons_Air: 301, Year: '2009-01-04'},
+  {Total_Injured_Persons: 2237367, Total_Injured_Persons_Air: 301, Year: '2010-01-04'},
+  {Total_Injured_Persons: 2237367, Total_Injured_Persons_Air: 301, Year: '2011-01-04'},
+  {Total_Injured_Persons: 2237367, Total_Injured_Persons_Air: 301, Year: '2012-01-04'},
+  {Total_Injured_Persons: 2237367, Total_Injured_Persons_Air: 301, Year: '2013-01-04'},
+  {Total_Injured_Persons: 2237367, Total_Injured_Persons_Air: 301, Year: '2014-01-04'},
+  {Total_Injured_Persons: 2237367, Total_Injured_Persons_Air: 301, Year: '2015-01-04'},
+  {Total_Injured_Persons: 2237367, Total_Injured_Persons_Air: 301, Year: '2016-01-04'}];
+const output2 = [{Total_Injured_Persons: 2237367, Total_Injured_Persons_Air: 301, Year: '2010-01-04'}, 
+  {Total_Injured_Persons: 2237367, Total_Injured_Persons_Air: 301, Year: '2011-01-04'},
+  {Total_Injured_Persons: 2237367, Total_Injured_Persons_Air: 301, Year: '2012-01-04'}];
+
+
+describe('injuries', () => {
+  it('debería ser un objeto', () => {
+    expect(typeof injuries).toBe('object');
+  });
+  describe('injuries.obtenerAñosUnicos', () => {
+    it('debería ser una función', () => {
+      expect(typeof injuries.obtenerAñosUnicos).toBe('function');
+    });
+    it('debería retornar un array con los años únicos que presente la data', () => {
+      expect(injuries.obtenerAñosUnicos(input)).toEqual(output);
+    });
+  });
+  describe('injuries.recentYear', () => {
+    it('Deberia ser una funcion', () => {
+      expect(typeof injuries.recentYears).toBe('function');
+    });
+    it('Deberia retornar un array con los ultimos años de la data', () => {
+      expect(injuries.recentYears(input2)).toEqual(output2);
+    });
+  });
+  describe('injuries.filtrarRango', () => {
+    it('Deberia ser una funcion', () => {
+      expect(typeof injuries.filtrarRango).toBe('function');
+    });
+    it('Deberia retornar un array de la data en un rango de años', () => {
+      expect(injuries.filtrarRango(input2)).toEqual(output2);
+    });
+  });
+});
+
+/*
 describe('example', () => {
   it('is a function', () => {
     expect(typeof example).toBe('function');
@@ -9,4 +56,4 @@ describe('example', () => {
   it('returns `example`', () => {
     expect(example()).toBe('example');
   });
-});
+});*/
