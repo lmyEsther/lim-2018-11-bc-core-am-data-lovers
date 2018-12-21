425bcf56cf8e9f67ff0959d27bdc617fb2c48c5e
lmyEsther <esthercheremo@gmail.com>
2018-12-21 00:05:07 -0500

    data presentada en UI en forma de cards

--- src/main.js
+++ src/main.js
@@ -1,66 +1,55 @@
-let tableDataPrevia = document.getElementById('previa');
 const data = window.INJURIES;
-let dataRecent = injuries.recentYears(data);
-
-let listRecent = '';
-listRecent +=
-      '<tr>' +
-        '<th>Año</th>' +
-        '<th>Autopista</th>' +
-        '<th>Autobús</th>' +
-        '<th>Motocicletas</th>' +
-        '<th>Vehículos</th>' +
-        '<th>Bicicletas</th>' +
-        '<th>Peatones</th>' +
-        '<th>Camión</th>' +
-        '<th>Aéreo</th>' +
-        '<th>Férreo</th>' +
-        '<th>Marítimo y Fluvial</th>' +
-        '<th>Buques</th>' +
-        '<th>Yates</th>' +
-        '<th>Otros</th>';
-'</tr>';
-
-dataRecent.forEach(cant => {
-  listRecent += '<tr>';
-  listRecent += `<th>${parseInt(cant.Year)}</th>`;
-  listRecent += `<td>${parseInt(cant.Total_Injured_Persons_Highway)}</td>`;
-  listRecent += `<td>${cant.Total_Injured_Persons_Bus_Occupants +
-                            cant.Total_Injured_Persons_Commuter_Carrier}</td>`;
-  listRecent += `<td>${parseInt(cant.Total_Injured_Persons_Motorcyclists)}</td>`;
-  listRecent += `<td>${parseInt(cant.Total_Injured_Persons_Passenger_Car_Occupants)}</td>`;
-  listRecent += `<td>${parseInt(cant.Total_Injured_Persons_Pedalcyclists)}</td>`;
-  listRecent += `<td>${parseInt(cant.Total_Injured_Persons_Pedestrians)}</td>`;
-  listRecent += `<td>${parseInt(cant.Total_Injured_Persons_Truck_Occupants_Light) +
-                            parseInt(cant.Total_Injured_Persons_Truck_Occupants_Large)}</td>`;
-  listRecent += `<td>${parseInt(cant.Total_Injured_Persons_Air) +
-                            parseInt(cant.Total_Injured_Persons_General_Aviation) +
-                            parseInt(cant.Total_Injured_Persons_On_Demand_Air_Taxi) +
-                            parseInt(cant.Total_Injured_Persons_US_Air_Carrier)}</td>`;
-  listRecent += `<td>${parseInt(cant.Total_Injured_Persons_Railroad_Alone)}</td>`;
-  listRecent += `<td>${parseInt(cant.Total_Injured_Persons_Water) +
-                            parseInt(cant.Total_Injured_Persons_Water_Not_Related_To_Vessel_Casualties) +
-                            parseInt(cant.Total_Injured_Persons_Water_Vessel_Related)}</td>`;
-  listRecent += `<td>${parseInt(cant.Total_Injured_Persons_Freight_Vessel) +
-                            parseInt(cant.Total_Injured_Persons_Passenger_Vessel)}</td>`;
-  listRecent += `<td>${parseInt(cant.Total_Injured_Persons_Recreational_Boating)}</td>`;
-  listRecent += `<td>${parseInt(cant.Total_Injured_Persons_Employee_Or_Worker) +
-                            parseInt(cant.Total_Injured_Persons_Gas_Pipeline) +
-                            parseInt(cant.Total_Injured_Persons_Hazardous_Liquid_Pipeline) +
-                            parseInt(cant.Total_Injured_Persons_Industrial_Or_Other) +
-                            parseInt(cant.Total_Injured_Persons_Other_Incident) +
-                            parseInt(cant.Total_Injured_Persons_Other_Incidents) +
-                            parseInt(cant.Total_Injured_Persons_Pipeline)}</td>`;
-});
-
-tableDataPrevia.innerHTML = listRecent; // fin primera tabla de vista previa
-
-// selector de rango y filtrado por años que elija el usuario
+let sectionCard = document.getElementById('card');
+let tableDataPrevia = document.getElementById('previa');
+let dataRecent = injuries.recentYears(data).reverse();
 let selectYearStar = document.getElementById('añoInicio');
 let selectYearFinish = document.getElementById('añoFin');
-let btnFilterByRange = document.getElementById('btn-select-range')
+let btnFilterByRange = document.getElementById('btn-select-range');
 let arrAños = injuries.obtenerAñosUnicos(data);
 let dataTableFilter = document.getElementById('dataFilter');
+
+
+const cardCreater = (arr, section) => {
+  let cardData = '';
+
+  arr.forEach((cant => {
+    cardData += '<div class="col-md-3">' + 
+    '<div class="card">' + '<div class="card-block">' +
+    `<h3 class="card-title">${parseInt(cant.Year)}</h3>` +
+    '<ul class="list-unstyled">' + 
+      `<li>Urbano: ${parseInt(cant.Total_Injured_Persons_Highway) + 
+        cant.Total_Injured_Persons_Bus_Occupants +
+        cant.Total_Injured_Persons_Commuter_Carrier + 
+        parseInt(cant.Total_Injured_Persons_Motorcyclists) +
+        parseInt(cant.Total_Injured_Persons_Passenger_Car_Occupants) +
+        parseInt(cant.Total_Injured_Persons_Pedalcyclists) +
+        parseInt(cant.Total_Injured_Persons_Pedestrians)}</li>` +
+      `<li>Camión: ${parseInt(cant.Total_Injured_Persons_Truck_Occupants_Light) +
+        parseInt(cant.Total_Injured_Persons_Truck_Occupants_Large)}</li>` +
+      `<li>Aéreo: ${parseInt(cant.Total_Injured_Persons_Air) +
+        parseInt(cant.Total_Injured_Persons_General_Aviation) +
+        parseInt(cant.Total_Injured_Persons_On_Demand_Air_Taxi) +
+        parseInt(cant.Total_Injured_Persons_US_Air_Carrier)}</li>` + 
+      `<li>Férreo: ${parseInt(cant.Total_Injured_Persons_Railroad_Alone)}</li>` +
+      `<li>Maritimo y Fluvial: ${parseInt(cant.Total_Injured_Persons_Water) +
+        parseInt(cant.Total_Injured_Persons_Water_Not_Related_To_Vessel_Casualties) +
+        parseInt(cant.Total_Injured_Persons_Water_Vessel_Related)}</li>` +
+      `<li>Buques: ${parseInt(cant.Total_Injured_Persons_Freight_Vessel) +
+        parseInt(cant.Total_Injured_Persons_Passenger_Vessel)}</li>` +
+      `<li>Yates: ${parseInt(cant.Total_Injured_Persons_Recreational_Boating)}</li>` +
+      `<li>Otros: ${parseInt(cant.Total_Injured_Persons_Employee_Or_Worker) +
+        parseInt(cant.Total_Injured_Persons_Gas_Pipeline) +
+        parseInt(cant.Total_Injured_Persons_Hazardous_Liquid_Pipeline) +
+        parseInt(cant.Total_Injured_Persons_Industrial_Or_Other) +
+        parseInt(cant.Total_Injured_Persons_Other_Incident) +
+        parseInt(cant.Total_Injured_Persons_Other_Incidents) +
+        parseInt(cant.Total_Injured_Persons_Pipeline)}</li>` +
+    '</ul>' + '</div>' + '</div>' + '</div>';
+  }));
+  section.innerHTML = cardData;
+};
+
+cardCreater(dataRecent, tableDataPrevia);
 
 arrAños.forEach((año) => {
   selectYearStar.innerHTML += `<option value = ${año}>${año}</option>`;
@@ -68,62 +57,8 @@ arrAños.forEach((año) => {
 });
 
 btnFilterByRange.addEventListener('click', () => {
-
-  if (selectYearStar.value <= selectYearFinish.value) {
-
   let arrFilterByYear = injuries.totalInjuredPersonsByYear(data, selectYearStar.value, selectYearFinish.value);
-  
-  let listFilter = '';
-  listFilter += '<tr>' +
-  '<th>Año</th>' +
-  '<th>Autopista</th>' +
-  '<th>Autobús</th>' +
-  '<th>Motocicletas</th>' +
-  '<th>Vehículos</th>' +
-  '<th>Bicicletas</th>' +
-  '<th>Peatones</th>' +
-  '<th>Camión</th>' +
-  '<th>Aéreo</th>' +
-  '<th>Férreo</th>' +
-  '<th>Marítimo y Fluvial</th>' +
-  '<th>Buques</th>' +
-  '<th>Yates</th>' +
-  '<th>Otros</th>';
-  '</tr>';
-
-  arrFilterByYear.forEach((cant => {
-    listFilter += '<tr>';
-    listFilter += `<th>${parseInt(cant.Year)}</th>`;
-    listFilter += `<td>${parseInt(cant.Total_Injured_Persons_Highway)}</td>`;
-    listFilter += `<td>${cant.Total_Injured_Persons_Bus_Occupants +
-                              cant.Total_Injured_Persons_Commuter_Carrier}</td>`;
-    listFilter += `<td>${parseInt(cant.Total_Injured_Persons_Motorcyclists)}</td>`;
-    listFilter += `<td>${parseInt(cant.Total_Injured_Persons_Passenger_Car_Occupants)}</td>`;
-    listFilter += `<td>${parseInt(cant.Total_Injured_Persons_Pedalcyclists)}</td>`;
-    listFilter += `<td>${parseInt(cant.Total_Injured_Persons_Pedestrians)}</td>`;
-    listFilter += `<td>${parseInt(cant.Total_Injured_Persons_Truck_Occupants_Light) +
-                              parseInt(cant.Total_Injured_Persons_Truck_Occupants_Large)}</td>`;
-    listFilter += `<td>${parseInt(cant.Total_Injured_Persons_Air) +
-                              parseInt(cant.Total_Injured_Persons_General_Aviation) +
-                              parseInt(cant.Total_Injured_Persons_On_Demand_Air_Taxi) +
-                              parseInt(cant.Total_Injured_Persons_US_Air_Carrier)}</td>`;
-    listFilter += `<td>${parseInt(cant.Total_Injured_Persons_Railroad_Alone)}</td>`;
-    listFilter += `<td>${parseInt(cant.Total_Injured_Persons_Water) +
-                              parseInt(cant.Total_Injured_Persons_Water_Not_Related_To_Vessel_Casualties) +
-                              parseInt(cant.Total_Injured_Persons_Water_Vessel_Related)}</td>`;
-    listFilter += `<td>${parseInt(cant.Total_Injured_Persons_Freight_Vessel) +
-                              parseInt(cant.Total_Injured_Persons_Passenger_Vessel)}</td>`;
-    listFilter += `<td>${parseInt(cant.Total_Injured_Persons_Recreational_Boating)}</td>`;
-    listFilter += `<td>${parseInt(cant.Total_Injured_Persons_Employee_Or_Worker) +
-                              parseInt(cant.Total_Injured_Persons_Gas_Pipeline) +
-                              parseInt(cant.Total_Injured_Persons_Hazardous_Liquid_Pipeline) +
-                              parseInt(cant.Total_Injured_Persons_Industrial_Or_Other) +
-                              parseInt(cant.Total_Injured_Persons_Other_Incident) +
-                              parseInt(cant.Total_Injured_Persons_Other_Incidents) +
-                              parseInt(cant.Total_Injured_Persons_Pipeline)}</td>`;
-  }));
-
-  dataTableFilter.innerHTML = listFilter;
+  cardCreater(arrFilterByYear, dataTableFilter);
 });
 
-
+cardCreater(data, sectionCard);
